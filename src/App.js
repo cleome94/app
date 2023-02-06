1f4f8505e4aae3e6f7c236d105fdb899ea71fdc9
SYKim <cleome94@naver.com>
2023-02-06 14:57:23 +0900

    Commit Message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+// 파이어베이스에서 db 가져오기
+/* import NAME from './firebase.js' */
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore} from '@firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAC6rtE7G_VETeVKyTtgKCd1i67Z5hVq74",
+  authDomain: "pwa-to-do-97186.firebaseapp.com",
+  databaseURL: "https://pwa-to-do-97186-default-rtdb.firebaseio.com",
+  projectId: "pwa-to-do-97186",
+  storageBucket: "pwa-to-do-97186.appspot.com",
+  messagingSenderId: "233036299477",
+  appId: "1:233036299477:web:38d89ca922271c089b365c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
